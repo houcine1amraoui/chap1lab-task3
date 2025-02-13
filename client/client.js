3306627bd39a49a14cb18c9f059782fc72143a0e
houcine1amraoui <houcine1amraoui@gmail.com>
2025-02-13 20:49:40 +0100

    add headers content type issue

--- client/client.js
+++ client/client.js
@@ -10,13 +10,8 @@ const getAllUsers = async () => {
 const getOneUser = async () => {
   const res = await fetch("http://localhost:3000/users", {
     method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
     body: { id: "1" },
   });
   const user = await res.json();
   console.log(user);
 };
-
-getAllUsers();
