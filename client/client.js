fbfbf8e15e6a55fd00bbce198f3bb3814b0ebc14
houcine1amraoui <houcine1amraoui@gmail.com>
2025-02-15 09:49:10 +0100

    using regular function signature

--- client/client.js
+++ client/client.js
@@ -1,17 +1,17 @@
-const getAllUsers = async () => {
+async function getAllUsers() {
   const res = await fetch("http://localhost:3000/users");
   if (!res.ok) {
     throw new Error(`HTTP error! status: ${res.status}`);
   }
   const users = await res.json();
-  console.log(users);
-};
+  return users;
+}
 
-const getOneUser = async () => {
+async function getOneUser() {
   const res = await fetch("http://localhost:3000/users", {
     method: "POST",
     body: { id: "1" },
   });
   const user = await res.json();
-  console.log(user);
-};
+  return user;
+}
