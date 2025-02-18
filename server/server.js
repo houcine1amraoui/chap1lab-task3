4c8a559ca9da553eb638d20b033a5857f1d4aa0c
houcine1amraoui <houcine1amraoui@gmail.com>
2025-02-18 08:14:09 +0100

    updated

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
-import express, { json } from "express";
+import express from "express";
+
+import { fileURLToPath } from "url";
+import path from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
-app.use(json());
+app.use(express.json());
 
 export const users = [
   {
